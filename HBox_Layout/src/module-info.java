a9c120cd075938c1596d7596d7ba0ad6320edbcc
Kimberly <kjacques709@gmail.com>
2021-07-08 19:14:02 +0200

    Hundred-Sixth Program: Introduction to BorderPane Layout

---
+++ HBox_Layout/src/module-info.java
@@ -0,0 +1,6 @@
+module HBox.Layout {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ HBox_Layout/src/sample/Main.java
@@ -0,0 +1,36 @@
+/*
+Main.java
+------------------------------------------------------------------------------------------------------------------------
+Created by Eldr@e@
+------------------------------------------------------------------------------------------------------------------------
+Introduction to HBoxLayout
+------------------------------------------------------------------------------------------------------------------------
+Created on 22/06/2021
+------------------------------------------------------------------------------------------------------------------------
+Last Update on: 22/06/2021
+------------------------------------------------------------------------------------------------------------------------
+ */
+
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
